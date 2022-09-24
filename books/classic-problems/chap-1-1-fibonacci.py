55d0ac2861a7cdb17b39ff078bfc13f8a4ffcc3d
danielfontoura <danielfontoura@gmail.com>
2022-09-24 19:43:48 -0300

    1.1.5

--- books/classic-problems/chap-1-1-fibonacci.py
+++ books/classic-problems/chap-1-1-fibonacci.py
@@ -1,11 +1,11 @@
-from functools import lru_cache
-
-@lru_cache(maxsize=None)
-
 def fibonacci(n: int) -> int:
-    if n < 2:
-        return n        
-    return fibonacci(n -1) + fibonacci(n -2)
+    if n == 0:
+        return n
+    last: int = 0
+    next: int = 1
+    for _ in range(1, n):
+        last, next = next, last + next
+    return next
 
 if __name__ == "__main__":
-    print(fibonacci(250))
+    print(fibonacci(923))
