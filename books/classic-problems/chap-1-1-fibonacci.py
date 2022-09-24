99ab40862047f555725d1e77dcc9620364fd0851
danielfontoura <danielfontoura@gmail.com>
2022-09-24 19:33:19 -0300

    1.1.4

--- books/classic-problems/chap-1-1-fibonacci.py
+++ books/classic-problems/chap-1-1-fibonacci.py
@@ -1,11 +1,11 @@
-from typing import Dict
+from functools import lru_cache
 
-memoized: Dict[int, int] = {0:0, 1:1}
+@lru_cache(maxsize=None)
 
 def fibonacci(n: int) -> int:
-    if n not in memoized:
-        memoized[n] = fibonacci(n -1) + fibonacci(n -2)
-    return memoized[n]
+    if n < 2:
+        return n        
+    return fibonacci(n -1) + fibonacci(n -2)
 
 if __name__ == "__main__":
-    print(fibonacci(150))
+    print(fibonacci(250))
